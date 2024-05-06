d6c7113cc3e50c02d0d3313caf58c7e6019355fd
Monyz <mamy32011@gmail.com>
2024-05-06 16:53:44 +0300

    fix: fix ProfileFragment

--- app/src/main/java/com/example/refood/ProfileFragment.java
+++ app/src/main/java/com/example/refood/ProfileFragment.java
@@ -158,7 +158,7 @@ public class ProfileFragment extends Fragment {
             public void onSuccess(DocumentSnapshot documentSnapshot) {
                     User user = documentSnapshot.toObject(User.class);
                     usernameTextView.setText(user.getName().toString());
-                    if (user.avatar_path != "") {
+                    if (!Objects.equals(user.avatar_path, "")) {
                         StorageReference profileAvatarReference = storage.getReference(user.avatar_path);
                         profileAvatarReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                             @Override
